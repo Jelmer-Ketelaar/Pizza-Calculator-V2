734d547aaf4202265ae41a5d901241ccb4b60212
Jelmer Ketelaar <99053633@mydavinci.nl>
2020-02-12 09:11:30 +0100

    Added the old pizza-calculator

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// dit geeft aan welke pizza grootte de klant wilt hebben.
+let size = prompt("Welke soort grote pizza wilt u? U kunt kiezen tussen een kleine, medium of grote pizza");
+size = size.toLowerCase();
+if (size !== size) {
+    prompt("Deze pizza bestaat niet")
+}
+
+// dit geeft aan hoeveel pizza's de klant wilt.
+const aantal = prompt("Hoeveel " + size + " pizza's wilt u?");
+
+
+// dit geeft aan hoe duur de pizza's zijn.
+const klein = 8;
+const medium = 10;
+const groot = 12;
+
+// deze code geeft de berekening van het aantal small pizza's dat de klant wilt, kost.
+if (size === "kleine") {
+    document.write("kleine pizza" + ' ' + '&euro;' + klein * aantal);
+}
+
+else if (size === "klein") {
+    document.write("kleine pizza" + ' ' + '&euro;' + klein * aantal);
+}
+
+if (size === 'medium') {
+    document.write("medium pizza" + ' ' + '&euro;' + medium * aantal);
+}
+
+if (size === 'groot') {
+    document.write("grote pizza" + ' ' + '&euro;' + groot * aantal);
+}
+
+else if (size === "grote") {
+    document.write("grote pizza" + ' ' + '&euro;' + groot * aantal);
+}
+
+
