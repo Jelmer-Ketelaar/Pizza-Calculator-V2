44a83139c6979368ec5788dfc08b451258b498be
Jelmer Ketelaar <99053633@mydavinci.nl>
2020-02-13 14:02:37 +0100

    Added a shopping cart an an modal for information

--- js/script.js
+++ js/script.js
@@ -36,3 +36,11 @@
 // }
 //
 //
+
+function openRightMenu() {
+    document.getElementById("rightMenu").style.display = "block";
+  }
+  
+  function closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+  }
