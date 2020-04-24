abea3c41dc263db319830e64c4cb814bee07c79c
Jelmer <jelmer@ketelaar.me>
2020-04-24 11:19:48 +0200

    Tried to add images

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
   
 var pizzas = [
-	{name: 'Sinterklaas Pizza', price:12.00, img: 'sinterklaas-pizza.png', description: ''},
-	{name: 'Vegetarische Pizza', price:10.0 , img: 'marinara.png', description: ''},
-	{name: 'Salami Pizza', price: 11.30, img: 'salami-pizza.jpeg', description: ''},
-	{name: 'Pizza Margarita', price:8.95, img: 'margherita.jpg', description: ''},
-	{name: 'Pizza Chicken Kebab', price: 15.00, img: 'chicken-kebab.png', description: ''}
+	{name: 'Sinterklaas Pizza', price:12.00, img: 'img/pizza-sinterklaas.jpeg', description: ''},
+	{name: 'Vegetarische Pizza', price:10.0 , img: 'img/vega-pizza.jpg', description: ''},
+	{name: 'Salami Pizza', price: 11.30, img: 'img/salami-pizza.jpeg', description: ''},
+	{name: 'Pizza Margarita', price:8.95, img: 'img/margherita.jpg', description: ''},
 ];
 
 document.getElementById('pizza-list').innerHTML = '';
@@ -13,4 +12,10 @@ for (i = 0; i < pizzas.length; i++) {
   var li = document.createElement('LI');
   li.innerHTML= pizzas[i].name;
   document.getElementById('pizza-list').appendChild(li);
+}
+
+for (var a = 0; a < pizzas.length; a ++) {
+  var li = document.createElement('LI');
+  li.innerHTML = '<img src="'+pizzas[i].img+'">'
+  document.getElementById('img').appendChild(li);
 }
