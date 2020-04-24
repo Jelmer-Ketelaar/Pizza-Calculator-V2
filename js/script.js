85ff40959407ad3cf2cf62c3e6466e81920830d5
Jelmer Ketelaar <99053633@mydavinci.nl>
2020-04-24 12:26:04 +0200

    Added the pizza images

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
-var pizzas = [
-	{name: 'Sinterklaas Pizza', price:12.00, img: 'img/pizza-sinterklaas.jpeg', description: ''},
-	{name: 'Vegetarische Pizza', price:10.0 , img: 'img/vega-pizza.jpg', description: ''},
-	{name: 'Salami Pizza', price: 11.30, img: 'img/salami-pizza.jpeg', description: ''},
-	{name: 'Pizza Margarita', price:8.95, img: 'img/margherita.jpg', description: ''},
+const pizzas = [
+    {name: 'Sinterklaas Pizza', price: 12.00, img: 'img/pizza-sinterklaas.jpeg', description: ''},
+    {name: 'Vegetarische Pizza', price: 10.0, img: 'img/vega-pizza.jpg', description: ''},
+    {name: 'Salami Pizza', price: 11.30, img: 'img/salami-pizza.jpeg', description: ''},
+    {name: 'Pizza Margarita', price: 8.95, img: 'img/margherita.jpg', description: ''},
 ];
 
 document.getElementById('pizza-list').innerHTML = '';
 
+let li;
 for (i = 0; i < pizzas.length; i++) {
-  var li = document.createElement('LI');
-  li.innerHTML= pizzas[i].name;
-  document.getElementById('pizza-list').appendChild(li);
+    li = document.createElement('LI');
+    li.innerHTML = pizzas[i].name;
+    document.getElementById('pizza-list').appendChild(li);
 }
 
-for (var a = 0; a < pizzas.length; a ++) {
-  var li = document.createElement('LI');
-  li.innerHTML = '<img src="'+pizzas[i].img+'">'
-  document.getElementById('img').appendChild(li);
+
+for (let a = 0; a < pizzas.length; a++) {
+    li = document.createElement('LI');
+    li.innerHTML = '<img src="' + pizzas[a].img + '" alt="pizza-img">'
+    document.getElementById("pizza-list").appendChild(li);
 }
 
